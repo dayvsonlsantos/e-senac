5e4a0e63da760aca48e993bf99632a4942ec6e0a
Dayvson Lima <dayvsonlsantos@gmail.com>
2023-11-13 15:43:43 -0300

    feat: Primeiro Projeto React Native

---
+++ periodo03/mobile/first_project_ReactNative/first_project_ReactNative/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import Home from "./Components/Home";
+import Habilidades from "./Components/Habilidades";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+        ></Stack.Screen>
+        <Stack.Screen
+          name="Habilidades"
+          component={Habilidades}
+        ></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
